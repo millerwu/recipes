5b1848ef01d36ce79d0a157d87de34579a837abf
millerwu <wumiaoliang@gmail.com>
2017-11-02 18:15:52 +0800

    update

--- base/LogStream.h
+++ base/LogStream.h
@@ -7,6 +7,11 @@
 
 namespace fake
 {
+namespace detail {
+
+  const int kSmallBuffer = 4000;
+  const int kLargeBuffer = 4000*1000;
+
   template <int SIZE>
   class FixBuffer : Copyable
   {
@@ -22,31 +27,49 @@ namespace fake
 
     }
 
-    void append(const char* buffer, int length)
+    void append(const char* buffer, size_t length)
     {
-      int copy_length = 0;
-      if (SIZE-(cur_-data_) >= length) {
-        copy_length = length;
-      } else {
-        copy_length = SIZE-(cur_-data_);
+      if (avail()>length) {
+        memcpy(cur_, buffer, length);
+        cur_ += length;
       }
-      memcpy(cur_, buffer, copy_length);
-      cur_ += copy_length;
     }
 
+    const char* data() { return data_; }
+    int length() const { return static_cast<int>(cur_-data_); }
+
+    //directly control buffer
+    char* current() { return cur_; }
+    int avail() const { return static_cast<int>(end()-cur_); }
+    void add(int len) { cur_ += len; }
+
+    void reset() { cur_ = data_; }
+    void bzero() { ::bzero(data_, sizeof(data_)); }
+
+
   private:
+    const char* end() { return data_ + sizeof(data_);}
     char data_[SIZE];
     char* cur_;
   };
-
+}
   class LogStream : NonCopyable
   {
+    typedef LogStream self;
   public:
+    typedef detail::FixBuffer<detail::kSmallBuffer> Buffer;
+    self& operator<<(bool v)
+    {
+      buffer_.append(v ? "1": "0", 1);
+      return *this;
+    }
+
+  private:
+    Buffer buffer_;
     LogStream();
     ~LogStream();
-
-
   };
+
 }
 
 #endif //FAKE_BASE_LOGSTREAM_H
